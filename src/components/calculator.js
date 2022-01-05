d4e5a2603d2f731c2e74fdf4a8f22e81c201c93c
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-05 08:15:36 -0500

    Fix linters

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -9,30 +9,32 @@ class Calculator extends React.Component {
   }
 
   render() {
-    return (<div className="calculator">
-      <div id="calc-output">0</div>
-      <CalcButton text="AC" className="calc-number" />
-      <CalcButton text="+/-" className="calc-number" />
-      <CalcButton text="%" className="calc-number" />
-      <CalcButton text="/" className="calc-operation" />
-      <CalcButton text="7" className="calc-number" />
-      <CalcButton text="8" className="calc-number" />
-      <CalcButton text="9" className="calc-number" />
-      <CalcButton text="x" className="calc-operation" />
-      <CalcButton text="4" className="calc-number" />
-      <CalcButton text="5" className="calc-number" />
-      <CalcButton text="6" className="calc-number" />
-      <CalcButton text="-" className="calc-operation" />
-      <CalcButton text="1" className="calc-number" />
-      <CalcButton text="2" className="calc-number" />
-      <CalcButton text="3" className="calc-number" />
-      <CalcButton text="+" className="calc-operation" />
-      <CalcButton text="0" className="calc-zero" />
-      <CalcButton text="." className="calc-number" />
-      <CalcButton text="=" className="calc-operation" />
+    return (
+      <div className="calculator">
+        <div id="calc-output">0</div>
+        <CalcButton text="AC" className="calc-number" />
+        <CalcButton text="+/-" className="calc-number" />
+        <CalcButton text="%" className="calc-number" />
+        <CalcButton text="/" className="calc-operation" />
+        <CalcButton text="7" className="calc-number" />
+        <CalcButton text="8" className="calc-number" />
+        <CalcButton text="9" className="calc-number" />
+        <CalcButton text="x" className="calc-operation" />
+        <CalcButton text="4" className="calc-number" />
+        <CalcButton text="5" className="calc-number" />
+        <CalcButton text="6" className="calc-number" />
+        <CalcButton text="-" className="calc-operation" />
+        <CalcButton text="1" className="calc-number" />
+        <CalcButton text="2" className="calc-number" />
+        <CalcButton text="3" className="calc-number" />
+        <CalcButton text="+" className="calc-operation" />
+        <CalcButton text="0" className="calc-zero" />
+        <CalcButton text="." className="calc-number" />
+        <CalcButton text="=" className="calc-operation" />
 
-    </div>)
-  };
+      </div>
+    );
+  }
 }
 
 export default Calculator;
