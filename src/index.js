bb5fff64383f01bd059cd5d5be038d6943c441cf
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-12 14:05:28 -0500

    Display pages properly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
 import Nav from './Nav';
 import Home from './Home';
 import Quote from './Quote';
@@ -10,13 +10,13 @@ import Quote from './Quote';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-    <Route path="math-magicians/" element={<Nav/>} >
-      <Route index element={<Home/>}/>
-      <Route path='çalculator' element={<App/>} />
-      <Route path='quotes' element={<Quote/>}/>
-    </Route>
-    </Routes>
+      <Routes>
+        <Route path="math-magicians/" element={<Nav />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<App />} />
+          <Route path="quotes" element={<Quote />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
